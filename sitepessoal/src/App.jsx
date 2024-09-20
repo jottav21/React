a378ca372fc2e9e1a600958a80f730bed1beb3b6
J.victt <102590669+Victornunes21@users.noreply.github.com>
2024-09-19 23:08:09 -0300

    isso vai dar ruim

--- sitepessoal/src/App.jsx
+++ sitepessoal/src/App.jsx
@@ -5,9 +5,15 @@ function App() {
   
 
   return (
-    <div>
-      ola mundo
-    </div>
+      <div class="header">
+        <h3>João Victor</h3>
+      <nav>
+        <a href="#Portifolio">Portifolio</a>
+        <a href="#Curiosidades">Curiosidades</a>
+        <a href="#Contato">Contatos</a>
+      </nav>
+</div>
+
 
   )
 }
